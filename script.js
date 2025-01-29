00989a9262aac085389149818ca84abef65f8afa
sanjusahu2002 <132829320+sanjusahu2002@users.noreply.github.com>
2025-01-29 11:00:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+        let count = 0;
+        let counter = document.getElementById('counter');
+        let incrementBtn = document.getElementById('incrementBtn');
+
+        incrementBtn.addEventListener('click',()=>{
+            alert(count);
+            count++;
+            counter.innerHTML = count;
+        })
